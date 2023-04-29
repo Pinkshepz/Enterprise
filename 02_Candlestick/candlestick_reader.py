1ffa74e297eef4ff5a3e08974a66e9631fc1a01f
Pinkshepz <82048274+Pinkshepz@users.noreply.github.com>
2023-04-29 01:09:32 +0000

    29 APR 23

--- 02_Candlestick/candlestick_reader.py
+++ 02_Candlestick/candlestick_reader.py
@@ -1,19 +1,53 @@
 """Compute candlestick chart into tabular format"""
 
 # Import libraries
+import os
 from PIL import Image
 from numpy import asarray
+import matplotlib.pyplot as plt
+import ast
 
-path = "02_Candlestick/input/01.jpeg"
+# Get file path
+PATH_INPUT = "02_Candlestick/input/"
+FILE_INPUT = next(os.walk(PATH_INPUT), (None, None, []))[2]
+
 # load the image and convert into np.array
-img = Image.open(path)
+IMG = Image.open(PATH_INPUT + FILE_INPUT[0])
 
 # asarray() class is used to convert
 # PIL images into NumPy arrays
-numpydata = asarray(img)
+IMG_ARR = asarray(IMG)
 
-# <class 'numpy.ndarray'>
-print(type(numpydata))
+# Shape
+HEIGHT, WIDTH, RGB = IMG_ARR.shape
 
-#  shape
-print(numpydata.shape)
+# Check Color Profile
+CHECK_COLOR_PROFILE = True
+
+if CHECK_COLOR_PROFILE is True:
+    color_profile = {}
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            pixel = str(IMG_ARR[i][j])
+            if pixel in color_profile:
+                color_profile[pixel] += 1
+            else:
+                color_profile[pixel] = 1
+    color_profile = ({k: v for k, v in sorted(color_profile.items(), key=lambda item: item[1])})
+
+    fig, ax = plt.subplots()
+
+    var_plot = color_profile[-10:-1]
+
+    var_x = var_plot.keys()
+    print(var_x)
+    # var_count = var_plot.values()
+    # var_colors = [(r / 255, g / 255, b / 255) for r, g, b in ast.literal_eval(var_x)]
+
+    # ax.bar(var_x, var_y, label=bar_labels, color=bar_colors)
+
+    # ax.set_ylabel('fruit supply')
+    # ax.set_title('Fruit supply by kind and color')
+    # ax.legend(title='Color')
+
+    # plt.show()
