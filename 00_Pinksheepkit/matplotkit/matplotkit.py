4c6fd462c53a4a34181f19b374edb48ff11a37b9
Pinkshepz <82048274+Pinkshepz@users.noreply.github.com>
2023-05-01 12:01:58 +0000

    1 MAY 23

---
+++ 00_Pinksheepkit/matplotkit/matplotkit.py
@@ -0,0 +1,33 @@
+"""Module containing decorators for formatting Matplotlib charts in my favorite design"""
+
+# Imports
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# Configure fonts
+FONT_PATH = "/workspaces/Enterprise/00_Pinksheepkit/fonts/poppins/Poppins-{0}.ttf"
+HEADING = "Bold"
+LABEL = "Regular"
+CONTENT = "Light"
+
+h_font = {"fontproperties": fm.FontProperties(fname=FONT_PATH.format(HEADING)),
+          "size": 12}
+l_font = {"fontproperties": fm.FontProperties(fname=FONT_PATH.format(LABEL)),
+          "size": 10}
+c_font = {"fontproperties": fm.FontProperties(fname=FONT_PATH.format(CONTENT)),
+          "size": 8}
+
+# alpha decorator (Simple two-axis charts)
+def alpha_format(function):
+    """Alpha decorator (Simple two-axis charts) accept 3 arguments: title, x_label, y_label"""
+    def wrapper(title, x_label, y_label): # title, x_label, y_label
+        fig, ax = function()
+        plt.rcParams['figure.dpi'] = 100
+        ax.spines[['right', 'top']].set_visible(False)
+        ax.set_title(title, **h_font)
+        ax.set_xlabel(x_label, **l_font)
+        ax.set_ylabel(y_label, **l_font)
+        ax.tick_params(axis='both', which='major', **c_font)
+        plt.show()
+        return fig, ax
+    return wrapper
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# XAU-Forex-Analysis
+# **Project 23/07 - Enterprise Project**
 Analysis the relationship between XAU, FOREX, and major economic events.
 
-1. Scrap economic parameters (e.g. CPI, PMI, and unemployment rate) from Forex Factory website
-2. Clean, analyze and visualize data to explore deep relationship and patterns among data
-3. Use API to fetch XAU and FOREX market prices in 1h timeframe
-4. Analyze the relationship between economic patameters and market signals (e.g. price change and volume)
+## **00 Pinksheepkit**
+Mini repository for storing assets (i.e. fonts) and modules (i.e. custom decorators toolkit)
+
+## **01 Scrapper**
+Scraps economic parameters (e.g. CPI, PMI, and unemployment rate) from Forex Factory website
+
+## **02 Candlestick**
+Reads candlestick chart image and returns price data of each candlestick in tabular form
