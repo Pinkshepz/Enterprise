8db85ded92381f181208658c3006ede5b0a2992e
Pinkshepz <82048274+Pinkshepz@users.noreply.github.com>
2023-06-01 06:48:33 +0000

    1 JUN 23

---
+++ 04_Print/app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask,render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET"])
+def index():
+    # Display data on index.html
+    return render_template("index.html", birthday_output=[None])
