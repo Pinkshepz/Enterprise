0ee754a45c26aac3cb3171af23d72c4c4e4fca0c
Pinkshepz <82048274+Pinkshepz@users.noreply.github.com>
2023-06-02 13:54:21 +0000

    2 JUN 23

--- 04_Print/candlestick_chart/chart_generator.py
+++ 04_Print/candlestick_chart/chart_generator.py
@@ -1,9 +1,24 @@
+""" chart_generator.py module
+    Produce candlestick chart image and forex factory table in weekly timeframe
+
+    Input:
+    1. .csv file path of candlestick chart
+    2. .csv file path of forex economic data
+
+    Out:
+    1. .png image of candlestick chart
+    2. .csv file of FOREX economic data parsed by week
+
+    This is a cat -> 🐈
+"""
+
 # Imports
+import datetime as dt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-import datetime as dt
+import matplotlib.dates as mdates
 from tqdm import tqdm
 
 # Configure fonts
@@ -28,96 +43,204 @@ plt.rcParams['figure.dpi'] = 300
 # Root path
 ROOT = '/workspaces/Enterprise/04_Print/static/data/'
 
-# Path of two datasets
-PATH_XAUUSD = '/workspaces/Enterprise/03_Analytics/input/XAUUSD-1H_220103_230503.csv'
-PATH_FOREX = '/workspaces/Enterprise/03_Analytics/input/FOREX_311222_131020.csv'
+# Function for data preparation and cleaning to DataFrame
+def prepare_data(path_xauusd, path_forex):
+    """ Prepare and clean DataFrame 
 
-# Read csv
-DF_XAUUSD = pd.read_csv(PATH_XAUUSD, parse_dates=['time']).drop(columns=['Unnamed: 0'])
-DF_FOREX = pd.read_csv(PATH_FOREX, parse_dates=[['date', 'time']]).drop(columns=['Unnamed: 0'])
+    Args
+    1. XAUUSD.csv path
+    2. FOREX.csv path
 
-DF_XAUUSD.rename(columns={'time': 'datetime'}, inplace=True)
-DF_FOREX.rename(columns={'date_time': 'datetime'}, inplace=True)
-
-# Change dataype of DF_XAUUSD
-for col in DF_XAUUSD.columns[1:]:
-    DF_XAUUSD[col] = DF_XAUUSD[col].astype('float64')
-
-# Change dataype of DF_FOREX
-DF_FOREX["datetime"] = pd.to_datetime(DF_XAUUSD["datetime"], format='%d-%b-%Y, %H:%M:%S')
-DF_FOREX['currency'] = DF_FOREX['currency'].astype('category')
-DF_FOREX['impact'] = DF_FOREX['impact'].astype('category')
-DF_FOREX['event'] = DF_FOREX['event'].astype('category')
-
-# Define function to handle various nember formats i.e. 100K 50%
-def handle_number_format(number: str) -> list:
-    """ Separate value and unit of given number formats
-    
-    Arg:
-        1. number: str i.e. 6.5%, 100K, 2.8
-
-    Out:
-        1. list: <[float, str | None]> i.e. [6.5, "%"], [100, "K"], [2.8, None]
- 
-    This is a cat -> 🐈
+    Out
+    1. pd.DataFrame of XAUUSD
+    2. pd.DataFrame of FOREX
     """
 
-    # case 1: na
-    if number == '':
-        return [None, None]
-    number = str(number)
-    # case 2: % value
-    if (number[-1] == '%') & (number.lstrip('-')[0].isnumeric() == True):
-        return [float(number.rstrip('%')), '%']
-    # case 3: 1K 1M 1B 1T unit
-    if (number[-1].isalpha() == True) & (number.lstrip('-')[0].isnumeric() == True):
-        return [float(str(number)[:-1]), number[-1]]
-    # case 4: ordinary numbers
-    if number.replace(',', '').replace('.', '').lstrip('-').isnumeric() == True: 
-        return [float(number.replace(',', '')), None]
-    # case 5: non-numbers
-    return [number, None]
+    # Read csv
+    df_xauusd = pd.read_csv(PATH_XAUUSD, parse_dates=['time']).drop(columns=['Unnamed: 0'])
+    df_forex = pd.read_csv(PATH_FOREX, parse_dates=[['date', 'time']]).drop(columns=['Unnamed: 0'])
 
-# Apply data format function
-for col in DF_FOREX.columns[5:]:
-    DF_FOREX[col] = DF_FOREX[col].fillna('').apply(handle_number_format)
+    df_xauusd.rename(columns={'time': 'datetime'}, inplace=True)
+    df_forex.rename(columns={'date_time': 'datetime'}, inplace=True)
+
+    # Change dataype of df_xauusd
+    for col in df_xauusd.columns[1:]:
+        df_xauusd[col] = df_xauusd[col].astype('float64')
+
+    # Change dataype of df_forex
+    df_forex["datetime"] = pd.to_datetime(df_xauusd["datetime"], format='%d-%b-%Y, %H:%M:%S')
+    df_forex['currency'] = df_forex['currency'].astype('category')
+    df_forex['impact'] = df_forex['impact'].astype('category')
+    df_forex['event'] = df_forex['event'].astype('category')
+
+    # Define function to handle various nember formats i.e. 100K 50%
+    def handle_number_format(number: str) -> list:
+        """ Separate value and unit of given number formats
+        
+        Arg:
+            1. number: str i.e. 6.5%, 100K, 2.8
+
+        Out:
+            1. list: <[float, str | None]> i.e. [6.5, "%"], [100, "K"], [2.8, None]
+        """
+
+        # case 1: na
+        if number == '':
+            return [None, None]
+        number = str(number)
+        # case 2: % value
+        if (number[-1] == '%') & (number.lstrip('-')[0].isnumeric() is True):
+            return [float(number.rstrip('%')), '%']
+        # case 3: 1K 1M 1B 1T unit
+        if (number[-1].isalpha() is True) & (number.lstrip('-')[0].isnumeric() is True):
+            return [float(str(number)[:-1]), number[-1]]
+        # case 4: ordinary numbers
+        if number.replace(',', '').replace('.', '').lstrip('-').isnumeric() is True:
+            return [float(number.replace(',', '')), None]
+        # case 5: non-numbers
+        return [number, None]
+
+    # Apply data format function
+    for col in df_forex.columns[5:]:
+        df_forex[col] = df_forex[col].fillna('').apply(handle_number_format)
+
+    return df_xauusd, df_forex
+
 
 # Function for plot candlestick chart
-def candlestick(df_price):
-    # Matplotlib figure
+def candlestick_chart(df_xauusd):
+    """ Plot candlestick chart
+
+    Args:
+    1. df_xauusd DataFrame
+    2. df_forex DataFrame
+
+    Out:
+    1. .png image of candlestick chart parsed by week
+    """
+
+    # create new DataFrame
     fig, ax = plt.subplots(1, 1, figsize=(15, 5))
 
-    # Up stores bullish candles and down stores bearish candles
-    up = df_price[df_price.close >= df_price.open]
-    down = df_price[df_price.close < df_price.open]
+    # create candlestick features
+    ## 1. bullish-bearish color
+    df_xauusd['bb'] = df_xauusd['%Change'].apply(
+        lambda x: C_BULL if x >= 0 else C_BEAR)
+
+    ## 2.candle-wick patterns xauusd
+    df_xauusd['candle'] = df_xauusd[['open', 'high', 'low', 'close']].apply(
+        lambda x: sorted(x)[2] - sorted(x)[1], axis=1)
+    df_xauusd['w+'] = df_xauusd[['open', 'high', 'low', 'close']].apply(
+        lambda x: sorted(x)[3] - sorted(x)[2], axis=1)
+    df_xauusd['w-'] = df_xauusd[['open', 'high', 'low', 'close']].apply(
+        lambda x: sorted(x)[0] - sorted(x)[1], axis=1)
+    df_xauusd['bottom'] = df_xauusd[['open', 'high', 'low', 'close']].apply(
+        lambda x: sorted(x)[1], axis=1)
+    df_xauusd['upper'] = df_xauusd[['open', 'high', 'low', 'close']].apply(
+        lambda x: sorted(x)[2], axis=1)
     
     # Setting width of candlestick elements
-    width = .7
-    width2 = .07
-    
-    # Plotting bullish candles
-    ax.bar(up.index, up.close-up.open, width, bottom=up.open, color=C_BULL) # candle
-    ax.bar(up.index, up.high-up.close, width2, bottom=up.close, color=C_BULL) # upper wick
-    ax.bar(up.index, up.low-up.open, width2, bottom=up.open, color=C_BULL) # lower wick
-    
-    # Plotting bearish candles
-    ax.bar(down.index, down.close-down.open, width, bottom=down.open, color=C_BEAR) # candle
-    ax.bar(down.index, down.high-down.open, width2, bottom=down.open, color=C_BEAR) # upper wick
-    ax.bar(down.index, down.low-down.close, width2, bottom=down.close, color=C_BEAR) # lower wick
-    
-    # Format charts
+    width_candle = .03
+    width_wick = .003
+
+    # Plotting df_xauusd prices of the stock
+    ax.bar(df_xauusd.datetime, df_xauusd['candle'], width_candle,
+           bottom=df_xauusd['bottom'], color=df_xauusd['bb'])
+    ax.bar(df_xauusd.datetime, df_xauusd['w+'], width_wick,
+           bottom=df_xauusd['upper'], color=df_xauusd['bb'])
+    ax.bar(df_xauusd.datetime, df_xauusd['w-'], width_wick,
+           bottom=df_xauusd['bottom'], color=df_xauusd['bb'])
+
+    # Format ticks
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
+
+    ax.xaxis.set_minor_locator(mdates.HourLocator(byhour=(0, 6, 12, 18)))
+    ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
+
+    ax.xaxis.grid(True, which='minor')
+    ax.yaxis.grid(True)
+
+    ax.tick_params(axis="x", which="major", pad=0)
+
     plt.xticks(**C_FONT)
+    plt.xticks(minor=True, **C_FONT)
     plt.yticks(**C_FONT)
-    ax.set_xlabel('Time (24 Hours)', **L_FONT)
-    ax.set_xticks(np.arange(0, 24, 2))
-    ax.set_xlim([0, 23])
+
+    ax.set_xlabel('Datetime (24H UTC+7)', **L_FONT)
     ax.set_ylabel('Price (USD)', **L_FONT)
+
+    # Format spines and grid
+    ax.spines[['right']].set_visible(False)
+    ax.spines[['top']].set_visible(False)
+    ax.set_axisbelow(True)
     ax.grid(True)
     
     # Save figure
-    f_name = f'ROOT{df_price.iloc[0, 0].strftime("%y%m%d")}_price.png'
+    f_name = f'{ROOT}candle_{df_xauusd.iloc[0, 0].strftime("%y_%U")}.png'
     plt.savefig(f_name)
 
     return 0
 
-# Loop on each column to export candlestick chart image and tabular csv
+
+# Function for parsing DataFrame weekly
+def parse_data(df_xauusd, df_forex):
+    """ Parse xauusd and forex data weekly
+
+    Args:
+    1. df_xauusd DataFrame
+    2. df_forex DataFrame
+
+    Out:
+    1. list of weekly parsed df_xauusd DataFrame
+    2. list of weekly parsed df_forex DataFrame
+    """
+
+    # Get first day in xauusd DataFrame
+    origin_date = df_xauusd.iloc[0, 0]
+    terminal_date = df_xauusd.iloc[-1, 0]
+
+    # Get first and last days of the first week
+    week_start = origin_date - dt.timedelta(days=origin_date.weekday()) # time 05:00:00
+    week_end = week_start + dt.timedelta(days=6) # time 05:00:00
+
+    # Parse weekly
+    week_delta = dt.timedelta(weeks=1)
+
+    # List of weekly parsed data
+    parse_xauusd = []
+
+    # Loop parsing weekly
+    while week_start < terminal_date:
+        # Append parsed DataFrame
+        parse_xauusd.append(
+            df_xauusd[(df_xauusd.datetime >= week_start) & (df_xauusd.datetime < week_end)]
+        )
+
+        # Increment week day interval
+        week_start += week_delta
+        week_end += week_delta
+    
+    return parse_xauusd
+
+
+# Run this module
+if __name__ == '__main__':
+    print("Initiating...")
+    # Path of two datasets
+    PATH_XAUUSD = '/workspaces/Enterprise/03_Analytics/input/XAUUSD-1H_220103_230503.csv'
+    PATH_FOREX = '/workspaces/Enterprise/03_Analytics/input/FOREX_311222_131020.csv'
+
+    # Get DataFrame
+    DF_XAUUSD, DF_FOREX = prepare_data(PATH_XAUUSD, PATH_FOREX)
+
+    # Parse date
+    parse_xauusd = parse_data(DF_XAUUSD, DF_FOREX)
+
+    print("Rendering images...")
+    count = 0
+    for parse_df in tqdm(parse_xauusd):
+        candlestick_chart(parse_df)
+        count += 1
+
+    print("Successfully render {count} images...")
